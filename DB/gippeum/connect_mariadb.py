636424f919b59286b954e94d506430661421d18a
LEEPPEUM <leeppeum2@gmail.com>
2020-08-03 11:46:05 +0900

    default

--- DB/gippeum/connect_mariadb.py
+++ DB/gippeum/connect_mariadb.py
@@ -1,8 +1,8 @@
 import pymysql
-db = pymysql.connect(host='localhost',port=3306,user='root',passwd='tiger',db='yojulabdb',charset='utf8',autocommit=True)
+db = pymysql.connect(host='192.168.0.41',port=3306,user='scott',passwd='tiger',db='finalproject',charset='utf8',autocommit=True)
 
 cursor = db.cursor(pymysql.cursors.DictCursor)
-cursor.execute("SELECT* from economic")
+cursor.execute("SELECT * from DB_leegippeum")
 data = cursor.fetchall()
 for infor in data:
     print("economic : %s " % infor['title'])
