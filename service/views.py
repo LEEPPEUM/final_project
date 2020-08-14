9816db410c3854947d8a666997db0c47ee05d79c
JunO-93 <dlwnsdh17@naver.com>
2020-08-14 07:36:21 +0000

    juno

--- service/views.py
+++ service/views.py
@@ -2,6 +2,13 @@ from django.shortcuts import render
 from django.shortcuts import HttpResponse
 import pymysql
 import pandas as pd
+import numpy as np
+import tensorflow as tf
+import sklearn
+from sklearn.preprocessing import MinMaxScaler
+from tensorflow import keras
+from keras.models import load_model
+
 # Create your views here.
 pddata=[]
 pddatasets=[]
@@ -59,30 +66,77 @@ def home(request):
     db.close()
 
     pdDatasets = pd.DataFrame(datasets)
-    testdata = pdDatasets[pdDatasets['ldCode']==pnu_10]
+    
+   
+    
+    #ont-hot encoding
+    testdata = pd.get_dummies(pdDatasets,columns=['prposArea1','lndcgrCode','tpgrphHgCode','tpgrphFrmCode','roadSideCode'])
+    testdata = testdata[testdata['ldCode']==pnu_10]    
     testdata = testdata.reset_index()
+    
 
     try :
         testdata['pblntfPclnd'] = testdata[testdata['pblntfPclnd']] = jiga # 입력받은 공시지가로 모든 컬럼 변경
     except :
         print('ignore')
+        
+    #print('지가대치',testdata)
+    
+      
 
+    ### predict form에 맞게 데이터 변환
+
+    testdata= testdata[['pnu', 'ldCode', 'pblntfPclnd', 'lndpclAr', 'pclndIndex', 'pclndChgRt', 'juso', 'trafficAmount', 'distance', 'prposArea1_11', 'prposArea1_13', 'prposArea1_14', 'prposArea1_15', 'prposArea1_16', 'prposArea1_21', 'prposArea1_22', 'prposArea1_23', 'prposArea1_32', 'prposArea1_33', 'prposArea1_41', 'prposArea1_42', 'prposArea1_43', 'prposArea1_44', 'prposArea1_62', 'prposArea1_63', 'prposArea1_64', 'prposArea1_71', 'prposArea1_81', 'lndcgrCode_01', 'lndcgrCode_02', 'lndcgrCode_03', 'lndcgrCode_04', 'lndcgrCode_05', 'lndcgrCode_08', 'lndcgrCode_09', 'lndcgrCode_10', 'lndcgrCode_11', 'lndcgrCode_12', 'lndcgrCode_13', 'lndcgrCode_14', 'lndcgrCode_15', 'lndcgrCode_16', 'lndcgrCode_17', 'lndcgrCode_18', 'lndcgrCode_19', 'lndcgrCode_20', 'lndcgrCode_21', 'lndcgrCode_22', 'lndcgrCode_23', 'lndcgrCode_24', 'lndcgrCode_25', 'lndcgrCode_27', 'lndcgrCode_28', 'tpgrphHgCode_00', 'tpgrphHgCode_01', 'tpgrphHgCode_02', 'tpgrphHgCode_03', 'tpgrphHgCode_04', 'tpgrphHgCode_05', 'tpgrphFrmCode_00', 'tpgrphFrmCode_01', 'tpgrphFrmCode_02', 'tpgrphFrmCode_03', 'tpgrphFrmCode_04', 'tpgrphFrmCode_05', 'tpgrphFrmCode_06', 'tpgrphFrmCode_07', 'tpgrphFrmCode_08', 'roadSideCode_00', 'roadSideCode_01', 'roadSideCode_02', 'roadSideCode_03', 'roadSideCode_04', 'roadSideCode_05', 'roadSideCode_06', 'roadSideCode_07', 'roadSideCode_08', 'roadSideCode_09', 'roadSideCode_10', 'roadSideCode_11', 'roadSideCode_12']]
+    
+    nct = testdata[['lndpclAr','pblntfPclnd','pclndIndex','pclndChgRt','trafficAmount','distance']]
+    ct = testdata.drop(['lndpclAr','pblntfPclnd','pclndIndex','pclndChgRt','trafficAmount','distance','pnu','juso','ldCode',],axis=1)
+    
+    
+    # #scaling
+ 
+    scaler = MinMaxScaler()    
+    for i in nct:
+        nct[i]=scaler.fit_transform(nct[i].values.reshape(-1,1))
+    
+
+    # 넘파이 변환
+
+    ctnp = ct.to_numpy()
+    nctnp = nct.to_numpy()
+    
+    
+    pred_dataset = np.concatenate((nctnp,ctnp),axis=1)
+      
+    print(nctnp)
+   
+    ### 모델로드 & predict
+    from keras.models import load_model    
+
+    new_model = load_model('model_DL.h5')
+    
+    result_y = []
+    result_y =new_model.predict_classes([pred_dataset])
+    grade=[]
+    
+    for i in result_y:
+        if result_y[i]==0 :
+            grade+='A'
+        elif result_y[i]==1 :
+            grade+='B'
+        elif result_y[i]==2 :
+            grade+='C' 
+        elif result_y[i]==3 :
+            grade+='D'
+            
 
     #다시 dict type으로   
     testdata_to_dict = testdata.to_dict('list')  
-    mylist = zip(testdata_to_dict['juso'],testdata_to_dict['pblntfPclnd'],testdata_to_dict['pnu']) 
+    mylist = zip(testdata_to_dict['juso'],testdata_to_dict['pblntfPclnd'],grade) 
     context_ = {
         'mylist':mylist,
     }  
 
-
-    # 남은 작업 : 입력받은 공시지가로 가져온 데이터셋(pdDatasets) 속성에 다 매칭하여 바꿔주고
-
-    # 모델 받으면 모델에 넣어준다. 이 때 딥러닝이므로 레코드 하나하나를 numpy화 해야한다.     
-    
-
-    return render(request,'service/home.html',context=context_)
-    # return render(request,'service/home.html',context=data)
+    return render(request,'service/home.html',context=context_)   
 
 
 def form(request):    
